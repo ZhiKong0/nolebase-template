c0077dc9f347b7d11e10e263350c85d6776d6453
ZhiKong0 <1510794770@qq.com>
2026-03-01 20:57:30 +0800

    修复 GitHub Pages 域名和仓库链接配置

--- metadata/index.ts
+++ metadata/index.ts
@@ -7,11 +7,11 @@ export const siteDescription = '记录回忆，知识和畅想的地方'
 export const include = ['笔记', '生活']
 
 /** Repo */
-export const githubRepoLink = 'https://github.com/nolebase/nolebase'
+export const githubRepoLink = 'https://github.com/ZhiKong0/nolebase-template'
 /** Discord */
 export const discordLink = 'https://discord.gg/XuNFDcDZGj'
 
 /** 无协议前缀域名 */
-export const plainTargetDomain = 'nolebase.ayaka.io'
+export const plainTargetDomain = 'zhikong0.github.io/nolebase-template'
 /** 完整域名 */
 export const targetDomain = `https://${plainTargetDomain}`
